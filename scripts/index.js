55d7768c391ebc6f72458ce73cdf7e7552611096
Svetlana Saykova <svetlanasaykova@gmail.com>
2024-12-28 17:19:35 +0200

    change

--- scripts/index.js
+++ scripts/index.js
@@ -1,9 +1,41 @@
+import { initialCards } from './cards.js';
+
 // @todo: Темплейт карточки
+const cardTemplate = document.querySelector('#card-template').content;
 
 // @todo: DOM узлы
+const cardsList = document.querySelector('.places__list');
 
 // @todo: Функция создания карточки
 
+function createCard(cardContent) {
+    const cardElement = cardTemplate.querySelector('.card').cloneNode(true);
+    const cardTitle = cardElement.querySelector('.card__title');
+    const cardImage = cardElement.querySelector('.card__image');
+
+    cardTitle.textContent = cardContent.name;
+    cardImage.src = cardContent.link;
+    cardImage.alt = cardContent.name;
+
+
 // @todo: Функция удаления карточки
 
+    const cardDeleteBtn = cardElement.querySelector('.card__delete-button');
+    cardDeleteBtn.addEventListener('click', function() {
+        cardElement.remove();
+});
+
+    return cardElement;
+}
+
+
 // @todo: Вывести карточки на страницу
+
+function addCards(initialCards) {
+    initialCards.forEach(card => {
+        const cardElement = createCard(card);
+
+        cardsList.append(cardElement);
+    });
+}
+addCards(initialCards);
